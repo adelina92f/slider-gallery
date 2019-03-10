47c12e78f7ba8b620eaef7f8e89028334fb7996d
adelina92f <adelina_92f@abv.bg>
2019-03-10 15:49:10 +0200

    slider

--- js/plugin/slider.js
+++ js/plugin/slider.js
@@ -43,14 +43,11 @@ createPoints:function(){
     this.settings.$pagingPoints = this.settings.$paging.find('span[data-image]');
 }, 
 bindEvents:function(){
-    this.settings.$control = this.$slider.find('.slider__control');
-    this.$control.on('click', function(e){
+    this.settings.$control = this.settings.$slider.find('.slider__control');
+    this.settings.$control.on('click', function(e){
         let action =$(this).data('action');
-        this.sliderAction(action);
-    })
-}, 
-sliderAction:function(action){
-    switch(action) {
+        let sliderAction = (() =>{
+        switch(action) {
         case 'next':
             this.next();
             console.log("next is action");
@@ -64,8 +61,13 @@ sliderAction:function(action){
         default:
             console.log('Unknow action, pleace be careful');
             break;
-    }
+        }
+        }
+    )})
 },
+//sliderAction: function(action){
+    
+//},
     next:function (){
         this.settings.$current = this.$slider.find('.slider__item.current');
         let $next = this.settings.$current.next();
@@ -88,8 +90,8 @@ sliderAction:function(action){
         this.settings.$current = $newCurrent;
         let current_index = this.settings.$current.data('image');
 
-        let $prev = this.getPrev(this.settings.$current);
-        let $next = this.getNext(this.settings.$current);
+        let $prev = this.getPrev();
+        let $next = this.getNext();
 
         $prev.addClass('prev');
         $next.addClass('next');
@@ -102,19 +104,24 @@ sliderAction:function(action){
     setCurrentPoint:function (index){
         this.settings.activePoint = $('span[data-image='+ index +']').addClass('active');
      },
-     getPrev:function($element){
-        let $prev = $element.prev();
+     getPrev:function(){
+       // let $prev = $element.prev();
+        //if(!this.isExist($prev)){
+        //    $prev = this.settings.$items.last(); 
+       // }
+        //return this.$prev;
+        let $prev = this.settings.$current.prev();
         if(!this.isExist($prev)){
-            $prev = this.settings.$items.last(); 
-        }
-        return this.$prev;
+              $prev = this.settings.$items.last(); 
+           }
+           return $prev;
     }, 
-    getNext:function ($element){
-        let $next = this.settings.$element.next();
-        if(!isExist($next)){
+    getNext:function (){
+        let $next = this.settings.$current.next();
+        if(!this.isExist($next)){
             $next = this.settings.$items.first(); 
         }
-        return this.$next;
+        return $next;
     },
     reset:function (){
         this.settings.$pagingPoints.removeClass('active');
@@ -123,6 +130,6 @@ sliderAction:function(action){
     },
     isExist:function($element){
         return $element.length !== 0;
-    }
+    },
 } 
 adiSlider.initSlider()
